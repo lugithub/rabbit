1b3705c31232b6f04e62ff8968435db56fca2ca9
lubin <lu.bin.lu.bin@gmail.com>
2019-05-26 00:41:54 -0400

    restoreAllMocks

--- src/testing-lib/hidden-message.test.tsx
+++ src/testing-lib/hidden-message.test.tsx
@@ -6,11 +6,15 @@ import HiddenMessage from './hidden-message';
 // NOTE: you do NOT have to do this in every test.
 // Learn more about Jest's __mocks__ directory:
 // https://jestjs.io/docs/en/manual-mocks
+
 jest.mock('react-transition-group', () => {
   return {
     CSSTransition: jest.fn(({ children, in: show }) => (show ? children : null))
   };
 });
+
+afterAll(() => jest.resetAllMocks());
+
 test('you can mock things with jest.mock', () => {
   const { getByText, queryByText } = render(
     <HiddenMessage initialShow={true} />
@@ -20,7 +24,12 @@ test('you can mock things with jest.mock', () => {
 
   expect(getByText('Hello world')).not.toBeNull();
 
-  CSSTransition.mockClear();
+  fireEvent.click(toggleButton);
+  expect(queryByText('Hello world')).toBeNull();
+
+  fireEvent.click(toggleButton);
+  expect(queryByText('Hello world')).not.toBeNull();
+
   fireEvent.click(toggleButton);
   expect(queryByText('Hello world')).toBeNull();
 });
