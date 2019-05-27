ceee96b97191d2b19688f1ab3c2913cca91d1de4
lubin <lu.bin.lu.bin@gmail.com>
2019-05-27 17:13:46 -0400

    emoj sense :smile:

--- src/learn-mock/cal.ts
+++ src/learn-mock/cal.ts
@@ -1,5 +1,7 @@
 import * as math from './math';
 
+// it was the :white_check_mark:
+
 export const doAdd = (a: number, b: number) => math.add(a, b);
 export const doSubtract = (a: number, b: number) => math.subtract(a, b);
 export const doMultiply = (a: number, b: number) => math.multiply(a, b);
