fb2200488c3dcd49e805d9b893c49615f402c195
lubin <lu.bin.lu.bin@gmail.com>
2019-05-29 23:29:34 -0400

    convert imperative api to declarative api using hooks

--- src/hooks/use-interval.ts
+++ src/hooks/use-interval.ts
@@ -1,10 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef(() => {});
+
   useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
     if (delay !== null) {
-      let id = setInterval(callback, delay);
+      let id = setInterval(tick, delay);
       return () => clearInterval(id);
     }
-  }, [callback, delay]);
+  }, [delay]);
 }
