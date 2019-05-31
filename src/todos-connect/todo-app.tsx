4a793dccf4c7bd0ac7df39e29ade7cf602805f3a
lubin <lu.bin.lu.bin@gmail.com>
2019-05-30 23:19:30 -0400

    styles

--- src/todos-connect/todo-app.tsx
+++ src/todos-connect/todo-app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AddTodo from './containers/add-todo';
 import TodoList from './containers/todo-list';
 import VisibilityFilters from './containers/visibility-filters';
+import './styles.css';
 
 export default function TodoApp() {
   return (
