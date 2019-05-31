12cc69d9e472cebe863427991ff1d62b2c602dca
lubin <lu.bin.lu.bin@gmail.com>
2019-05-31 18:06:13 -0400

    enable redux-devtools

--- src/todos-connect/configure-store.ts
+++ src/todos-connect/configure-store.ts
@@ -10,4 +10,8 @@ export interface State {
   visibilityFilter: VISIBILITY_FILTERS;
 }
 
-export default createStore(rootReducer);
+export default createStore(
+  rootReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
