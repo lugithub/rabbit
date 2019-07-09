877ab39a0fe694d5a01066906618707c1d1e8c6a
lubin <lu.bin.lu.bin@gmail.com>
2019-07-08 23:00:37 -0400

    reimplement Todo with the useDispatch hook

--- src/todos-hook/containers/todo.tsx
+++ src/todos-hook/containers/todo.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import cx from 'classnames';
 import { toggleTodo } from '../actions';
 
 interface TodoProps {
   todo: { id: number; content: string; completed: boolean };
-  toggleTodo: typeof toggleTodo;
 }
 
-const Todo = ({ todo, toggleTodo }: TodoProps) => (
-  <li className="todo-item" onClick={toggleTodo.bind(null, todo.id)}>
-    {todo && todo.completed ? '👌' : '👋'}{' '}
-    <span
-      className={cx(
-        'todo-item__text',
-        todo && todo.completed && 'todo-item__text--completed'
-      )}
-    >
-      {todo.content}
-    </span>
-  </li>
-);
+const Todo = ({ todo }: TodoProps) => {
+  const dispatch = useDispatch();
 
-export default connect(
-  null,
-  { toggleTodo }
-)(Todo);
+  return (
+    <li
+      className="todo-item"
+      onClick={dispatch.bind(null, toggleTodo(todo.id))}
+    >
+      {todo && todo.completed ? '👌' : '👋'}{' '}
+      <span
+        className={cx(
+          'todo-item__text',
+          todo && todo.completed && 'todo-item__text--completed'
+        )}
+      >
+        {todo.content}
+      </span>
+    </li>
+  );
+};
+
+export default Todo;
